9eda521704ebccf55865ec03980c4483d6d8d126
seyeonlee3838 <seyeonlee3838@gmail.com>
2025-07-29 00:34:12 +0900

    Update exercises.py

--- module3_functions/exercises.py
+++ module3_functions/exercises.py
@@ -1,5 +1,11 @@
 # Module 3 – Functions
-def square(x):
+
+def square(x: float) -> float:
+    """Return the square of a number x."""
     return x * x
 
-print("Square of 4 is", square(4))
+if __name__ == "__main__":
+    # Example usages
+    print("Square of 4 is", square(4))
+    print("Square of -3 is", square(-3))
+    print("Square of 0.5 is", square(0.5))
